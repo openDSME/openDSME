04dd3626285203734ff74d966ccb82b53b918d2d
Maximilian Köstler <maximilian.koestler@tuhh.de>
2016-07-13 13:36:00 +0200

    Enable expiration again for testing.

--- dsmeLayer/gtsManager/GTSManager.cc
+++ dsmeLayer/gtsManager/GTSManager.cc
@@ -163,7 +163,7 @@ fsmReturnStatus GTSManager::stateIdle(GTSEvent& event) {
                 }
                 else if(it->getIdleCounter() > dsme.getMAC_PIB().macDSMEGTSExpirationTime) {
                     LOG_DEBUG("DEALLOCATE: Due to expiration");
-                    continue; // TODO: Remove when it works again!
+                    //continue; // TODO: Remove when it works again!
                 }
 
                 LOG_DEBUG("slot to deallocate found!");
