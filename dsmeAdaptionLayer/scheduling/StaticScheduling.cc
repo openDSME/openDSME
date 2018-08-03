bc0a1f3f22d3985ebd8805a9b57ae78230983985
Florian Meyer <clu5013@h3a-wks-erda.ti5.tu-harburg.de>
2018-08-03 15:07:02 +0200

    initial

---
+++ dsmeAdaptionLayer/scheduling/StaticScheduling.cc
@@ -0,0 +1,60 @@
+/*
+ * openDSME
+ *
+ * Implementation of the Deterministic & Synchronous Multi-channel Extension (DSME)
+ * described in the IEEE 802.15.4-2015 standard
+ *
+ * Authors: Florian Kauer <florian.kauer@tuhh.de>
+ *          Maximilian Koestler <maximilian.koestler@tuhh.de>
+ *          Sandrina Backhauss <sandrina.backhauss@tuhh.de>
+ *
+ * Based on
+ *          DSME Implementation for the INET Framework
+ *          Tobias Luebkert <tobias.luebkert@tuhh.de>
+ *
+ * Copyright (c) 2015, Institute of Telematics, Hamburg University of Technology
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include "./StaticScheduling.h"
+
+#include "../../../dsme_platform.h"
+#include "../../dsmeLayer/DSMELayer.h"
+#include "../../mac_services/dataStructures/IEEE802154MacAddress.h"
+#include "../../mac_services/pib/MAC_PIB.h"
+#include "../DSMEAdaptionLayer.h"
+
+
+void StaticScheduling::multisuperframeEvent() {
+}
+
+
+GTSSchedulingDecision StaticScheduling::getNextSchedulingAction(uint16_t address) {
+    return NO_SCHEDULING_ACTION; 
+}
+
+} /* namespace dsme */
