16118b36be321927d07442fd071480ab731b644c
Florian Meyer <fl.meyer@tuhh.de>
2019-04-28 23:52:45 +0200

    added tps-future

---
+++ dsmeAdaptionLayer/scheduling/TPSFuture.h
@@ -0,0 +1,82 @@
+/*
+ * openDSME
+ *
+ * Implementation of the Deterministic & Synchronous Multi-channel Extension (DSME)
+ * described in the IEEE 802.15.4-2015 standard
+ *
+ * Authors: Florian Kauer <florian.kauer@tuhh.de>
+ *          Maximilian Koestler <maximilian.koestler@tuhh.de>
+ *          Sandrina Backhauss <sandrina.backhauss@tuhh.de>
+ *
+ * Based on
+ *          DSME Implementation for the INET Framework
+ *          Tobias Luebkert <tobias.luebkert@tuhh.de>
+ *
+ * Copyright (c) 2015, Institute of Telematics, Hamburg University of Technology
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef TPSFUTURE_H_
+#define TPSFUTURE_H_
+
+#include "./GTSScheduling.h"
+
+namespace dsme {
+
+class DSMEAdaptionLayer;
+
+struct TPSFTxData : GTSSchedulingData {
+    TPSFTxData();
+
+    float avgIn; // TODO no float!
+    uint16_t multisuperframesSinceLastPacket;
+};
+
+class TPSFuture : public GTSSchedulingImpl<TPSFTxData, GTSRxData> {
+public:
+    TPSFuture(DSMEAdaptionLayer& dsmeAdaptionLayer) : GTSSchedulingImpl(dsmeAdaptionLayer), alpha(0) {
+    }
+
+    virtual void multisuperframeEvent() override;
+    // virtual uint8_t registerIncomingMessage(uint16_t address) override;
+    void setAlpha(float alpha);
+    void setMinFreshness(uint16_t freshness);
+    void setUseHysteresis(bool useHysteresis);
+    void setFutureLength(uint16_t multisuperframes); 
+    
+    GTSSchedulingDecision getNextSchedulingAction(uint16_t address) override; 
+
+private:
+    float alpha{0}; // TODO no float
+    uint16_t minFreshness{0xFFFF};
+    bool useHysteresis{true};
+    uint16_t futureLength{5}; 
+};
+
+} /* namespace dsme */
+
+#endif /* TPSFUTURE_H_ */
