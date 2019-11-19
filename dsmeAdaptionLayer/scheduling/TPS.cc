cd56c2a29f774d5111704ef01fd5f57be2ae4421
Ivonne Andrea Mantilla Gonzalez <azahhara@yahoo.com>
2019-11-19 13:33:41 +0100

    some adds to prepareMsgIfAnny and TPS

--- dsmeAdaptionLayer/scheduling/TPS.cc
+++ dsmeAdaptionLayer/scheduling/TPS.cc
@@ -97,10 +97,18 @@ void TPS::multisuperframeEvent() {
         data.avgIn = data.messagesInLastMultisuperframe * alpha + data.avgIn * (1 - alpha);
 
         uint8_t slots = this->dsmeAdaptionLayer.getMAC_PIB().macDSMEACT.getNumAllocatedGTS(data.address, Direction::TX);
-        uint8_t packets_per_slot = this->dsmeAdaptionLayer.getMAC_PIB().helper.getSymbolsPerSlot() / (127*2 + this->dsmeAdaptionLayer.getMAC_PIB().helper.getAckWaitDuration() + const_redefines::macLIFSPeriod + PRE_EVENT_SHIFT);
+
+        //LengthFrameInSymbols = Preamble +SFD + PHR + PSDU (PHYPayload)
+        //Preamble = 8 symbols;
+        //SFD = 2 symbols;
+        //PHR = 2 symbols;
+        //PSDU = MHR + MACPayload + MFR;
+
+        uint8_t packets_per_slot = this->dsmeAdaptionLayer.getMAC_PIB().helper.getSymbolsPerSlot() / (6 + 127)*2 + this->dsmeAdaptionLayer.getMAC_PIB().helper.getAckWaitDuration() + const_redefines::macLIFSPeriod + PRE_EVENT_SHIFT);
             /* '-> calculate number of packets per slot with assumption of maximum packet size and maximum acknowledgement wait duration -> THIS CAN BE DONE MUCH BETTER */ 
+
         LOG_DEBUG("Packets per slot: " << (int)packets_per_slot);
-        float error = data.avgIn / packets_per_slot - slots;
+        float error = (data.avgIn / packets_per_slot) - slots;
 
         int8_t change = 0;
         if(useHysteresis) {
